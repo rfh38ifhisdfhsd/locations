09521e2d06620afe349e0146e3781d80fc4961d8
rfh38ifhisdfhsd <94389216+rfh38ifhisdfhsd@users.noreply.github.com>
2021-11-15 21:48:58 +0500

    Add files via upload

---
+++ loc_cache.lua
@@ -0,0 +1,16 @@
+
+positions = {
+    {
+        map = "de_vertigo", 
+        name = "Fake duck",
+        stand = {-603.58428955078, -1305.7740478516, 11665.504882813},
+        eye_pos = {-12.52, 50.59, 0},
+        weapon = "weapon_molotov",
+        description = 'text about the description',
+        grenade = {
+            ['jump'] = true,
+            ['run'] = 25, 
+        }
+    },
+}
+return positions
